61615e41d33bcf90397652776ca4b210837c39a8
Harsh <harshp062@gmail.com>
2024-03-19 18:11:31 +0000

    first commit

---
+++ src/main/java/com/hp/EmployesApicrudApplication.java
@@ -0,0 +1,14 @@
+package com.hp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class EmployesApicrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployesApicrudApplication.class, args);
+	}
+
+}
